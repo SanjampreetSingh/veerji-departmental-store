a23f3c1f7ae4e8b949287bf50a58007328e69625
Sanjampreet Singh <you@example.com>
2021-11-02 22:14:49 +0530

    User payment model

--- src/components/users/user-payment-card/UserPaymentCard.jsx
+++ src/components/users/user-payment-card/UserPaymentCard.jsx
@@ -1,14 +1,25 @@
+import UserPaymentModal from "./UserPaymentModal"
+
 export default function UserPaymentCard() {
   return (
-    <div className="card text-center">
-      <h4 className="card-header bg-white fw-bold">Payment</h4>
-      <div className="card-body">
-        <h1 className="card-title my-4">
-          ₹ <b>100</b>
-        </h1>
-        <div className="border-top my-1" />
-        <button className="btn btn-primary my-1">Make Payment</button>
+    <>
+      <div className="card text-center">
+        <h4 className="card-header bg-white fw-bold">Payment</h4>
+        <div className="card-body">
+          <h1 className="card-title my-4">
+            ₹ <b>100</b>
+          </h1>
+          <div className="border-top my-1" />
+          <button
+            className="btn btn-primary my-1"
+            data-bs-toggle="modal"
+            data-bs-target="#paymentModal"
+          >
+            Make Payment
+          </button>
+        </div>
       </div>
-    </div>
+      <UserPaymentModal />
+    </>
   )
 }
---
+++ src/components/users/user-payment-card/UserPaymentModal.jsx
@@ -0,0 +1,42 @@
+export default function UserPaymentModal(params) {
+  return (
+    <div
+      class="modal fade"
+      id="paymentModal"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="paymentModal"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header text-center">
+            <h4 className="modal-title  bg-white fw-bold">Payment</h4>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
