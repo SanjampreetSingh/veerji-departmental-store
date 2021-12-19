fae1965f6a0358eaa5d5c89b8c5e18c324dadbfa
Sanjampreet Singh <you@example.com>
2021-12-19 23:40:58 +0530

    updated admin router protected

--- src/layouts/admin/AdminRouter.jsx
+++ src/layouts/admin/AdminRouter.jsx
@@ -1,21 +1,35 @@
+import { useEffect, useState } from "react"
 import { Route, Redirect } from "react-router-dom"
 import jwt_decode from "jwt-decode"
 
 import AdminLayout from "./AdminLayout"
 
 export default function AdminRouter({ component: Component, ...rest }) {
-  const token = localStorage?.getItem("access_token")
-  let decoded = null
-  if (token !== null) {
-    decoded = jwt_decode(token)
-  }
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  let token = localStorage?.getItem("refresh_token")
 
+  useEffect(() => {
+    if (token) {
+      let tokenExpiration = jwt_decode(token)?.exp
+      let dateNow = new Date()
+
+      if (tokenExpiration < dateNow.getTime() / 1000) {
+        setIsAuthenticated(false)
+      } else {
+        setIsAuthenticated(true)
+      }
+    } else {
+      setIsAuthenticated(false)
+    }
+  }, [])
+
+  const type = jwt_decode(token)?.type
   return (
     <Route
       {...rest}
       render={props =>
-        token ? (
-          decoded?.type === 1 ? (
+        isAuthenticated ? (
+          type === 1 ? (
             <AdminLayout>
               <Component {...props} />
             </AdminLayout>
