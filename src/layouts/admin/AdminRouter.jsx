c80119a000983b268bafe4285ebdd8536ce7d264
Sanjampreet Singh <you@example.com>
2021-12-19 23:14:59 +0530

    admin router protected

--- src/layouts/admin/AdminRouter.jsx
+++ src/layouts/admin/AdminRouter.jsx
@@ -1,16 +1,31 @@
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
+import jwt_decode from "jwt-decode"
 
 import AdminLayout from "./AdminLayout"
 
 export default function AdminRouter({ component: Component, ...rest }) {
+  const token = localStorage?.getItem("access_token")
+  let decoded = null
+  if (token !== null) {
+    decoded = jwt_decode(token)
+  }
+
   return (
     <Route
       {...rest}
-      render={props => (
-        <AdminLayout>
-          <Component {...props} />
-        </AdminLayout>
-      )}
+      render={props =>
+        token ? (
+          decoded?.type === 1 ? (
+            <AdminLayout>
+              <Component {...props} />
+            </AdminLayout>
+          ) : (
+            <Redirect to="/" />
+          )
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
     />
   )
 }
