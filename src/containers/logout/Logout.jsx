23e916642e60bbe8562c956e7a823a8af25a5a99
Sanjampreet Singh <you@example.com>
2021-12-21 01:05:16 +0530

    resolved a bug in logout

--- src/containers/logout/Logout.jsx
+++ src/containers/logout/Logout.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react"
 import { useHistory } from "react-router-dom"
+
 import { instance } from "../../services/ApiCall"
 
 export default function Logout() {
   const history = useHistory()
+  let token = localStorage?.getItem("refresh_token")
 
   useEffect(() => {
-    instance.post("user/logout/blacklist/", {
-      refresh_token: localStorage.getItem("refresh_token"),
-    })
-    localStorage.removeItem("access_token")
-    localStorage.removeItem("refresh_token")
-    instance.defaults.headers["Authorization"] = null
+    if (token) {
+      instance.post("user/logout/blacklist/", {
+        refresh_token: token,
+      })
+      localStorage.removeItem("access_token")
+      localStorage.removeItem("refresh_token")
+      instance.defaults.headers["Authorization"] = null
+    }
     history.push("/login")
   })
 
-  return <div> Logout </div>
+  return <></>
 }
